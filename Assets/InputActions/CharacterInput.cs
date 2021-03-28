7a33c1235580772c1bf26da73b4ebacaebb2a815
Dustin <dustin.behnke@t-online.de>
2021-03-28 17:43:24 +0200

    Imported animations, prepared player character, functional character movement (walking, sprinting, jumping), functional animations (walking, jumping)

---
+++ Assets/InputActions/CharacterInput.cs
@@ -0,0 +1,315 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputActions/CharacterInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @CharacterInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @CharacterInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""CharacterInput"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""6419a634-9b6a-437f-badb-6afae8363857"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""Button"",
+                    ""id"": ""1b3b22b8-ef28-495d-92d7-9987b7d1dcf6"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""3954ee61-5d7c-43f2-a029-6456da206001"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""Sprint"",
+                    ""type"": ""Button"",
+                    ""id"": ""510cfba9-8a92-46a8-8fb3-99eafeb7dd2e"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=2)""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""WASDKeys"",
+                    ""id"": ""0be7939f-aa8c-4cca-937c-9ace0d3c877c"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""c0642d87-2451-4ebf-9f0a-8212a222a349"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""1ec9d2bc-3d93-4aac-b4a8-831627641887"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""21d778c8-9068-40de-a0eb-d8adf68862de"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""ea560e5f-d06a-4692-9d2e-5b7dc7a9331a"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""ArrowKeys"",
+                    ""id"": ""dae14f09-9b6a-4202-9a85-46053a0d7033"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": ""Press(behavior=2)"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""39b0a087-a49a-4d20-892d-e6633a24b429"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""76ca1d9d-238c-4b7a-a21a-d46511061e71"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""34cc2b05-521d-4c8a-9a87-442e0b9ca272"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""bd0aec35-1822-4cb9-a56d-debfa7d048e4"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""425333f7-c04c-499f-9d82-07b36b569757"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""1f02d2ff-63df-4a75-a297-73db8fb820e2"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard and mouse"",
+                    ""action"": ""Sprint"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard and mouse"",
+            ""bindingGroup"": ""Keyboard and mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Movement = m_Player.FindAction("Movement", throwIfNotFound: true);
+        m_Player_Jump = m_Player.FindAction("Jump", throwIfNotFound: true);
+        m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Movement;
+    private readonly InputAction m_Player_Jump;
+    private readonly InputAction m_Player_Sprint;
+    public struct PlayerActions
+    {
+        private @CharacterInput m_Wrapper;
+        public PlayerActions(@CharacterInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Player_Movement;
+        public InputAction @Jump => m_Wrapper.m_Player_Jump;
+        public InputAction @Sprint => m_Wrapper.m_Player_Sprint;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnJump;
+                @Sprint.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSprint;
+                @Sprint.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSprint;
+                @Sprint.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnSprint;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
+                @Sprint.started += instance.OnSprint;
+                @Sprint.performed += instance.OnSprint;
+                @Sprint.canceled += instance.OnSprint;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    private int m_KeyboardandmouseSchemeIndex = -1;
+    public InputControlScheme KeyboardandmouseScheme
+    {
+        get
+        {
+            if (m_KeyboardandmouseSchemeIndex == -1) m_KeyboardandmouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard and mouse");
+            return asset.controlSchemes[m_KeyboardandmouseSchemeIndex];
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
+        void OnSprint(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] Camera _characterCamera;
+    [SerializeField] Transform _followedTransform;
+    private CharacterController _characterController;
+
+    private Vector3 _gravity = Physics.gravity;
+    private Vector3 _velocity;
+
+    private CharacterInput _characterInput;
+
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _groundMask;
+    private float _groundDistance = 0.1f;
+    private bool _isGrounded = false;
+
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private float _movementSpeed = 8;
+    [SerializeField] private float _jumpHeight = 2;
+    private float _sprintModifier = 2;
+    private bool _isMoving = false;
+    private bool _isJumping = false;
+    private bool _isSprinting =false;
+
+    private void Awake()
+    {
+        _characterCamera.gameObject.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        _characterController = gameObject.GetComponent<CharacterController>();
+
+        InitializeInput();
+    }
+
+    private void Update()
+    {
+        GroundCheck();
+
+        if (_isMoving)
+        {
+            float x = _characterInput.Player.Movement.ReadValue<Vector2>().x;
+            float z = _characterInput.Player.Movement.ReadValue<Vector2>().y;
+
+            float cam = _followedTransform.rotation.eulerAngles.x;
+            transform.rotation = Quaternion.Euler(0, _followedTransform.rotation.eulerAngles.y, 0);
+            _followedTransform.localEulerAngles = new Vector3(cam, 0, 0);
+
+            Vector3 moveX = new Vector3(_followedTransform.right.x, 0, _followedTransform.right.z) * x;
+            Vector3 moveZ = new Vector3(_followedTransform.forward.x, 0, _followedTransform.forward.z) * z;
+
+            Vector3 move = moveX + moveZ;
+
+            if (_isSprinting)
+            {
+                _characterController.Move(move * _movementSpeed * _sprintModifier * Time.deltaTime);
+
+                if (!_isJumping)
+                {
+                    _animator.SetFloat("horizontal", x * _sprintModifier);
+                    _animator.SetFloat("vertical", z * _sprintModifier);
+                }
+            }
+            else
+            {
+                _characterController.Move(move * _movementSpeed * Time.deltaTime);
+
+                if (!_isJumping)
+                {
+                    _animator.SetFloat("horizontal", x);
+                    _animator.SetFloat("vertical", z);
+                }
+            }
+        }
+        else
+        {
+            _animator.SetFloat("horizontal", 0);
+            _animator.SetFloat("vertical", 0);
+        }
+
+        ApplyGravity();
+    }
+
+    private void InitializeInput()
+    {
+        _characterInput = new CharacterInput();
+
+        _characterInput.Player.Movement.performed += context => Move();
+        _characterInput.Player.Jump.performed += context => Jump();
+        _characterInput.Player.Sprint.performed += context => Sprint();
+    }
+
+    private void Move()
+    {
+        _isMoving = !_isMoving;
+    }
+
+    private void GroundCheck()
+    {
+        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
+
+        if (_isGrounded)
+        {
+            _isJumping = false;
+        }
+    }
+
+    private void Jump()
+    {
+        GroundCheck();
+
+        if (_isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity.y);
+            _animator.SetTrigger("jump");
+            _isJumping = true;
+        }
+    }
+
+    private void Sprint()
+    {
+        _isSprinting = !_isSprinting;
+    }
+
+    private void ApplyGravity()
+    {
+        _velocity += _gravity * Time.deltaTime;
+        _characterController.Move(_velocity * Time.deltaTime);
+
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = 0;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _characterInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _characterInput.Disable();
+    }
+}
