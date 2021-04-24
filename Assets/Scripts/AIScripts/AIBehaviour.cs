ab10e66e8dba93e515b10ec9a66faebbc6e8beda
Dustin <dustin.behnke@t-online.de>
2021-04-24 14:26:00 +0200

    Implemented ai movement, the ai either moves randomly or follows waypoints

--- Assets/Scripts/AIScripts/AIBehaviour.cs
+++ Assets/Scripts/AIScripts/AIBehaviour.cs
@@ -3,21 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class AIBehaviour : MonoBehaviour
 {
+    private enum MovementType
+    {
+        Random,
+        Waypoint
+    };
+    
     [SerializeField] private GameObject _player;
     [SerializeField] private float _fieldOfView = 120;
     [SerializeField] private float _viewDistance = 15;
     [SerializeField] private bool _isAware = false;
+    [SerializeField] private float _moveRadius = 3;
+    [SerializeField] private MovementType _movementType = MovementType.Random;
 
+    [SerializeField] private Transform[] _waypoints;
     [SerializeField] private Transform _head;
 
+    private int _currentWaypointIndex = 0;
+    private Vector3 _movePosition;
     private NavMeshAgent _agent;
 
     public void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _movePosition = RandomMovePosition();
     }
 
     public void Update()
@@ -29,6 +42,7 @@ public class AIBehaviour : MonoBehaviour
         else
         {
             SearchForPlayer();
+            Move();
         }
     }
 
@@ -51,6 +65,55 @@ public class AIBehaviour : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void Move()
+    {
+        if (_movementType == MovementType.Random)
+        {
+            if (Vector3.Distance(transform.position, _movePosition) < 2)
+            {
+                _movePosition = RandomMovePosition();
+            }
+            else
+            {
+                _agent.SetDestination(_movePosition);
+            }
+        }
+        else
+        {
+            if (_waypoints.Length >= 2)
+            {
+                if (Vector3.Distance(_waypoints[_currentWaypointIndex].position, transform.position) < 2)
+                {
+                    if (_currentWaypointIndex == _waypoints.Length - 1)
+                    {
+                        _currentWaypointIndex = 0;
+                    }
+                    else
+                    {
+                        _currentWaypointIndex++;
+                    }
+                }
+                else
+                {
+                    _agent.SetDestination(_waypoints[_currentWaypointIndex].position);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("Use at least 2 waypoints for the AI's movement" + gameObject.name);
+            }
+        }
+    }
+
+    private Vector3 RandomMovePosition()
+    {
+        Vector3 randomPosition = (Random.insideUnitSphere * _moveRadius) + transform.position;
+        NavMeshHit navMeshHit;
+        NavMesh.SamplePosition(randomPosition, out navMeshHit, _moveRadius, -1);
+        
+        return new Vector3(navMeshHit.position.x, transform.position.y, navMeshHit.position.z);
     }
 
     public  void OnAware()
