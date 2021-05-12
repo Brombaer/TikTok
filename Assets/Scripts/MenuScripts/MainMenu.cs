2c9b6b9b8200d580af69441a9edba71654cade2a
Hayoon <hayoonee@gmail.com>
2021-05-12 10:50:20 +0200

    changed start menu type

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    private void Start()
+    [SerializeField] private GameObject menuObject;
+    [SerializeField] private CinemachineVirtualCamera cinemachine;
+
+    private void OnEnable()
     {
+
+        CharacterMovement.IsEnabled = false;
+        cinemachine.enabled = false;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
     }
 
     public void Playgame()
     {
-  
-        SceneManager.LoadScene(1);
+        cinemachine.enabled = true;
+        CharacterMovement.IsEnabled = true;
+        menuObject.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void Quitgame()
