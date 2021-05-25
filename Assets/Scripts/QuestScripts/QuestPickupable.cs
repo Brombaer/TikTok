d4e5f03c4109e9818a70d8371be0e51d38cacb7a
Hayoon <hayoonee@gmail.com>
2021-05-25 20:30:03 +0200

    QuestPickupable added

---
+++ Assets/Scripts/QuestScripts/QuestPickupable.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class QuestPickupable : MonoBehaviour
+{
+    public QuestEvent.ItemToComplete item;
+    public QuestManager qManager;
+    public QuestEvent qEvent;
+    public QuestButton qButton;
+
+
+    void Start()
+    {
+
+    }
+
+    public void PickupableItems()
+    {
+        qEvent.UpdateQuestEvent(QuestEvent.EventStatus.Done);
+        qButton.UpdateButton(QuestEvent.EventStatus.Done);
+        qManager.UpdateQuestsOnCompletion(qEvent);
+    }
+
+}
+
