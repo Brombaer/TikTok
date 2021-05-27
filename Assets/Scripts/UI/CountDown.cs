f19ea0b8ce6e522cc2d6e92a68725b0a0ef0002f
Hoang <halohoang@gmail.com>
2021-05-27 22:51:29 +0200

    Add in text format for timer

--- Assets/Scripts/UI/CountDown.cs
+++ Assets/Scripts/UI/CountDown.cs
@@ -13,6 +13,9 @@ public class CountDown : MonoBehaviour
     [SerializeField]
     private Image _image;
     private Color _targetColor;
+    [SerializeField]
+    private Text _text;
+
 
     //function
     public void Start()
@@ -21,27 +24,26 @@ public class CountDown : MonoBehaviour
         _currentTime = _startingTime;
     }
 
+
+
     public void Update()
     {
+        
         if (_currentTime != 0)
         {
+            
             _currentTime -= Time.deltaTime;
+            _text.text = _currentTime.ToString();
             _image.fillAmount = _currentTime / _startingTime;
             if (_currentTime < 10)
             {
                 _image.color = new Color32(255, 255, 0, 100);
             }
+
         }
-        //else if (_currentTime < 10)
-        //{
-        //    _image.color = new Color32(255, 0, 0, 100);
-        //    _image.fillAmount = _currentTime / _startingTime;
-        //}
         if (_currentTime == 0)
         {
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(1);
         }
     }
-
-
 }
